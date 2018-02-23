480f1e16bec07d3d35d67592764e97b4ecc9ccbe
Logan Campbell <lc002@email.uark.edu>
2018-02-23 03:55:56 -0600

    trying kotlin for the first time

---
+++ TestRun/src/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>)
+{
+
+    println("Enter a secret message: ")
+    // After running this line remember to press "Enter" after typing in some input
+    val msg = readLine() ?:""
+
+    print(msg)
+
+}
